0296d47f7451e89af6cb8b115d4eba3c7d57fdf2
Peter Bailey <peter.eldridge.bailey@gmail.com>
2014-04-07 18:45:48 -0700

    finish per-run data merging

--- mergeData.R
+++ mergeData.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 source('~/local/bin/pbutils.R')
+source('./read.R')
 
 require('data.table')
 
@@ -23,12 +24,23 @@ confSpace = na.omit(confSpace)
 
 confSpace$key = rowApply(confSpace, toKey)
 
-f = function(row){
-  ## Does merged data exist?
-    
-  ## If not, merge it.
-
+g = function(entry){
+  filename = file.path(entry$path, 'merged.Rsave')
+  if(-1 == file.access(filename)){
+    print(filename)
+    run(path=entry$path, saveResult=T)
+  } ##else cat('Merged data for', entry$date, 'already exists\n')
+  
   ## Read merged data.
+  e = new.env()
+  load(filename, envir=e)
+  as.list(e)
 }
-merged = rowApply(confSpace, f)
+
+f = function(conf){
+  ## Does merged data exist?  If not, merge it.
+  print(conf)
+  rowApply(entries[conf], g)
+}
+merged = mcrowApply(confSpace, f)
 names(merged) = confSpace$key
