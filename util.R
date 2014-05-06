d0ec250607948af063e2aaa4f27087a55a50740d
Peter Bailey <peter.eldridge.bailey@gmail.com>
2014-05-05 22:22:14 -0700

    power functions

---
+++ util.R
@@ -0,0 +1,29 @@
+powerTime = function(x){
+  ranks  = unique(x$rank)
+  powerCols = grep('_w$',names(x),value=T)
+  cols = c('start','duration',powerCols)
+  powerSteps =
+    lapply(ranks, function(r){
+      x = x[rank == r][order(uid), cols, with=F]
+      x = x[,list(start, power=pkg_w+dram_w+pp0_w)]
+      list(times = x$start, steps = stepfun(x$start, c(x$power,tail(x$power,1))))
+    })
+  times = sort(unique(unlist(lapply(powerSteps, '[[', 'times'))))
+  steps = lapply(powerSteps, '[[', 'steps')
+  powers =
+    as.data.table(cbind(start=times,
+          power=rowSums(as.data.table(lapply(steps, function(s) s(times))))))
+  return(powers)
+}
+
+rlePower = function(powers){
+  b = rle(powers$power)
+  newPowers = powers[1:b$lengths[1]]
+  row = 1 + b$lengths[1]
+  for(rle_index in 2:length(b$lengths)){
+    newPowers = rbind(newPowers, powers[row])
+    row = row + b$lengths[rle_index]
+  }
+  rbind(newPowers, as.data.table(list(start=max(powers$start), power=0)))
+  ##newPowers
+}
