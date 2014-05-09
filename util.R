206033c6cbf5344fc26aa8a0b25e9e02f34942c6
Peter Bailey <peter.eldridge.bailey@gmail.com>
2014-05-09 16:51:10 -0700

    timeslices

--- util.R
+++ util.R
@@ -28,7 +28,41 @@ rlePower = function(powers){
   ##newPowers
 }
 
-## return sets of indices for each timeslice
-timeslice = function(x, length=.01){
+## return sets of indices for each timeslice and how much of each task
+## is in each timeslice
+timeslice = function(sched, start=0, length=.01, n=1){
+  if(n != 1)
+    stop("Not implemented yet")
+
+  sched$end = sched[, start+weight]
+  maxStart = max(sched[,start])
+  if(start > maxStart)
+    stop('Start past last task')
   
+  sliceTimes = seq(start, maxStart, length)
+
+  f = function(sliceTime){
+    nextSlice = sliceTime + length
+### There's a package for finding interval overlap, but we only do this once.
+    int = sched[start >= sliceTime & end <= nextSlice, which=T] ## interior
+    ext = sched[start <  sliceTime & end >  nextSlice, which=T] ## exterior
+    ## left overlap
+    left = sched[start <  sliceTime & end >   sliceTime & end <= nextSlice,
+      which=T]
+    ## right overlap
+    right = sched[start >= sliceTime & start < nextSlice & end >  nextSlice,
+      which=T]
+
+    result = list(int=int, ext=ext, left=left, right=right)
+    
+###!@todo define overlap fractions (depends on overlap type). For
+###!dependent timeslices, we don't care about the right side, just the
+###!left side. We only get to decide configurations for tasks that
+###!start in the current timeslice (interior and right types); other
+###!tasks retain their previous configurations.
+    result
+  }
+  
+  result = lapply(sliceTimes, f)
+  result
 }
