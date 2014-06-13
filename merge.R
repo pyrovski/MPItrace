49856c25785062868d5fe091a2388cf66d0eaaac
Peter Bailey <peter.eldridge.bailey@gmail.com>
2014-06-12 18:37:49 -0700

    run multiple merges per script

--- merge.R
+++ merge.R
@@ -1,13 +1,16 @@
 #!/usr/bin/env Rscript
 #msub -l nodes=1
 
+options(mc.cores=16)
 args = commandArgs(trailingOnly=T)
-if(length(args) != 1){
+if(length(args) < 1){
   stop('expected path argument')
 }
-path = args[[1]]
+paths = args
 
 source('read.R')
 
-setwd(path)
-run(saveResult=T)
+lapply(paths, function(path){
+  tryCatch(run(path=path, saveResult=T, noReturn=T),
+           error = function(e) e)
+})
