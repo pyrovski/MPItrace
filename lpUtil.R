2eeb37043c9c0d1596f30e212f1a64bbb0be466e
Peter Bailey <peter.eldridge.bailey@gmail.com>
2014-07-01 16:41:32 -0700

    add lp utils

---
+++ lpUtil.R
@@ -0,0 +1,33 @@
+require('rjson')
+require('data.table')
+
+readLP = function(filename){
+  a = fromJSON(file=filename)
+
+  f = function(b){
+    arraySel = grep('[[]', names(b))
+    singleSel = setdiff(1:length(b), arraySel)
+    if(length(arraySel)){
+      arrayNames = gsub('[[].*[]]', '', names(b)[arraySel])
+      uArrayNames = unique(arrayNames)
+      arrayVars = nnapply(uArrayNames, function(name){
+        b = b[grep(paste(name, '[[]', sep=''), names(b))]
+        map = strsplit(gsub('[]]', '', names(b)), '[[]')
+        indices = sapply(map, '[[', 2)
+        b = rbindlist(lapply(b, as.data.table))
+        b$index = indices
+        b
+      })
+    } else
+      arrayVars = NULL
+    if(length(singleSel)){
+      singleVars = lapply(b[singleSel], as.data.table)
+    } else
+      singleVars = NULL
+    c(arrayVars, singleVars)
+  }
+  a$Solution[[2]]$Variable = f(a$Solution[[2]]$Variable)
+  a$Solution[[2]]$Constraint = f(a$Solution[[2]]$Constraint)
+  
+  a
+}
